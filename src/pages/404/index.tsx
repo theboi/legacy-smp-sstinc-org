f857e87c6c25ac7820ba58bdb6735516ed9db96d
Ryan The <ryan.the.2006@gmail.com>
2021-03-26 22:11:49 +0800

    More refactoring url page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import style from "./style.module.css";
 
-export default function Error404Page() {
+export default function ErrorPage(props: {status: number}) {
+  const msg = (() => {
+    switch (props.status) {
+      case 403: return "For Biden"
+      default: return "Page Not Found"
+    }
+  })()
+  const status = props.status ?? 404
+  const src = (() => {
+    switch (props.status) {
+      case 403: return "https://apnews.com/article/c142b9f08fa741c89bc26330d43e3abc"
+      default: return ""
+    }
+  })()
+
   return (
     <div className={style.main}>
-      <h1 className={style.errorCode}>404</h1>
-      <h3>Page Not Found</h3>
+      <img src={`/assets/errors/${status}.png`} alt={`${status}: ${msg}, from ${src}`} className={style.img} />
+      <h1 className={style.errorCode}>{status}</h1>
+      <h3>{msg}</h3>
     </div>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from "react";
-import { FaClipboard } from "react-icons/fa";
+import React from "react";
 
 import style from "./style.module.css";
-import { useRouter } from "next/router";
-
-let createdSuffix = "ERROR";
 
 export default function HomePage() {
   return (
-    <div className={style.content}>
+    <div className={style.main}>
       <h3>SST Inc Management Process (SMP)</h3>
       <p>
         Only for use by SST Inc Executive Committee (ExCo) and Board Of
@@ -18,85 +14,4 @@ export default function HomePage() {
       <p>Please use your SST Google Account to sign in.</p>
     </div>
   )
-}
-
-function SuccessPage(props: {}) {
-  let copiedPopupRef: HTMLDivElement;
-
-  const router = useRouter();
-
-  useEffect(() => {
-    // if (screen === `success`) {
-    navigator.clipboard.writeText(
-      `${window.location.href}${createdSuffix ?? "ERROR"}`
-    );
-    copiedPopupRef.className = `${style.copiedPopup} ${style.show}`;
-    setTimeout(() => {
-      if (copiedPopupRef !== null) {
-        copiedPopupRef.className = `${style.copiedPopup}`;
-      }
-    }, 5000);
-    // }
-  });
-
-  return (
-    <div className={style.content}>
-      <h3>Link created!</h3>
-      <p>Here's your new shortened link:</p>
-      <div
-        className={style.linkArea}
-        onClick={() => {
-          navigator.clipboard.writeText(
-            `${window.location.href}${createdSuffix ?? "ERROR"}`
-          );
-          copiedPopupRef.className = `${style.copiedPopup} ${style.show}`;
-          setTimeout(() => {
-            copiedPopupRef.className = `${style.copiedPopup}`;
-          }, 5000);
-        }}
-      >
-        <div
-          className={style.copiedPopup}
-          ref={(el) => {
-            copiedPopupRef = el;
-          }}
-        >
-          Copied!
-        </div>
-        <p>
-          {window.location.href}
-          <span className={style.suffixBold}>{createdSuffix}</span>
-          <FaClipboard className={style.clipboardIcon} />
-        </p>
-      </div>
-      <p>Copy this link and share it!</p>
-      <p>
-        You may remove links by requesting at ryan.theodore.2006@gmail.com.
-        (Until I find time to implement it in the admin console...)
-      </p>
-      <div>
-        <button
-          className={style.activeButton}
-          style={{ marginBottom: 10, marginTop: 30 }}
-          onClick={() => {
-            window.open(
-              `${window.location.href}${createdSuffix ?? "ERROR"}`,
-              "_blank"
-            );
-          }}
-        >
-          Test it out
-        </button>
-        <button
-          className={style.finalButton}
-          onClick={() => {
-            nameField = suffixField = deepLinkField = "";
-            router.push(`/admin`);
-          }}
-        >
-          Great! Another one!
-        </button>
-      </div>
-    </div>
-  );
 }
