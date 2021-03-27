c7728a9dde6d96644ccc04dfc5f784a093a7269b
Ryan The <ryan.the.2006@gmail.com>
2021-03-27 20:39:15 +0800

    Work on navbar and stuff

---
+++ src/components/field/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import style from "./style.module.css";
+
+export default function ThemeField(props: {
+  label: string;
+  placeholder: string;
+  onChange: (e) => void;
+  onKeyUp: (e) => void;
+  leftDetail?: React.ReactNode;
+  rightDetail?: React.ReactNode;
+}) {
+  return (
+    <div className={style.main}>
+      <label htmlFor={props.label}>{props.label}</label>
+      <div className={style.field}>
+        {props.leftDetail}
+        <input
+          id={props.label}
+          type="text"
+          placeholder={props.placeholder}
+          onChange={props.onChange}
+          onKeyUp={props.onKeyUp}
+        />
+        {props.rightDetail}
+      </div>
+    </div>
+  );
+}
--- src/pages/url/index.tsx
+++ src/pages/url/index.tsx
@@ -1,19 +1,20 @@
-import { useRouter } from 'next/router';
-import React, { useEffect, useRef, useState } from 'react';
-import { FaExclamationCircle } from 'react-icons/fa';
+import { useRouter } from "next/router";
+import React, { useEffect, useRef, useState } from "react";
+import { FaExclamationCircle } from "react-icons/fa";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-import style from './style.module.css'
-import { User, UserRole } from '../../model/user';
-import ThemeButton from '../../components/button';
+import style from "./style.module.css";
+import { User, UserRole } from "../../model/user";
+import ThemeButton from "../../components/button";
+import ThemeField from "../../components/field";
 
 let deepLinkField = "";
 let nameField = "";
 let suffixField = "";
 
-export default function UrlPage(props: {user: User}) {
+export default function UrlPage(props: { user: User }) {
   const router = useRouter();
 
   const [adminErrorMsg, setAdminErrorMsg] = useState(``);
@@ -25,110 +26,107 @@ export default function UrlPage(props: {user: User}) {
   nameField = suffixField = deepLinkField = "";
 
   useEffect(() => {
-    if (props.user?.role != UserRole.Admin) console.log("oop")
-  }, [props.user])
-  
-  function createUrl() {async () => {
-    if (nameField === "" || deepLinkField === "") {
-      setAdminErrorMsg(`Please fill in all required fields.`);
-      return;
-    } else if (
-      (await firebase
-        .firestore()
-        .collection("links")
-        .get()
-        .then((col) => {
-          for (const doc of col.docs) {
-            if (suffixField === doc.data().suffix) {
-              return true;
-            }
-          }
-          return;
-        })) === true
-    ) {
-      setAdminErrorMsg(`This link is already in use.`);
-      return;
-    } else if (
-      deepLinkField.search(
-        /(http|https):\/\/([a-zA-Z0-9][a-zA-Z0-9-]{0,}\.){1,}[a-zA-Z0-9]{2,}/
-      )
-    ) {
-      setAdminErrorMsg(`Invalid deep/long link provided`);
-      return;
-    } else if (
-      deepLinkField.includes(window.location.href) ||
-      deepLinkField.includes("bit.ly") ||
-      deepLinkField.includes("tinyurl.com") ||
-      deepLinkField.includes("goo.gl")
-    ) {
-      setAdminErrorMsg(`Site blacklisted`);
-      return;
-    } else if (
-      deepLinkField.includes("dQw4w9WgXcQ") ||
-      deepLinkField.includes("ub82Xb1C8os") ||
-      deepLinkField.includes("8ybW48rKBME")
-    ) {
-      window.location.href =
-        "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-      return;
-    }
+    if (props.user?.role != UserRole.Admin) console.log("oop");
+  }, [props.user]);
 
-    const genRandAlias = () => {
-      const characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-";
-      let randomAlias = "";
-      for (var i = 0; i < 4; i++) {
-        randomAlias += characters.charAt(
-          Math.floor(Math.random() * characters.length)
-        );
+  function createUrl() {
+    async () => {
+      if (nameField === "" || deepLinkField === "") {
+        setAdminErrorMsg(`Please fill in all required fields.`);
+        return;
+      } else if (
+        (await firebase
+          .firestore()
+          .collection("links")
+          .get()
+          .then((col) => {
+            for (const doc of col.docs) {
+              if (suffixField === doc.data().suffix) {
+                return true;
+              }
+            }
+            return;
+          })) === true
+      ) {
+        setAdminErrorMsg(`This link is already in use.`);
+        return;
+      } else if (
+        deepLinkField.search(
+          /(http|https):\/\/([a-zA-Z0-9][a-zA-Z0-9-]{0,}\.){1,}[a-zA-Z0-9]{2,}/
+        )
+      ) {
+        setAdminErrorMsg(`Invalid deep/long link provided`);
+        return;
+      } else if (
+        deepLinkField.includes(window.location.href) ||
+        deepLinkField.includes("bit.ly") ||
+        deepLinkField.includes("tinyurl.com") ||
+        deepLinkField.includes("goo.gl")
+      ) {
+        setAdminErrorMsg(`Site blacklisted`);
+        return;
+      } else if (
+        deepLinkField.includes("dQw4w9WgXcQ") ||
+        deepLinkField.includes("ub82Xb1C8os") ||
+        deepLinkField.includes("8ybW48rKBME")
+      ) {
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        return;
       }
-      firebase
-        .firestore()
-        .collection("links")
-        .get()
-        .then((col) => {
-          col.docs.map((doc) => {
-            if (randomAlias === doc.data().suffix) {
-              return genRandAlias();
-            }
-          });
-        });
-      return randomAlias;
-    };
 
-    if (authAdmins.includes(firebase.auth().currentUser.email)) {
-      const newRandomAlias = genRandAlias();
-      firebase
-        .firestore()
-        .collection(`links`)
-        .doc(suffixField || newRandomAlias)
-        .set({
-          name: nameField ?? "ERROR",
-          link: deepLinkField ?? "ERROR",
-          suffix: suffixField || newRandomAlias,
-          date: firebase.firestore.Timestamp.fromDate(new Date()),
-        })
-        .then(() => {
-          createdSuffix = suffixField || newRandomAlias;
-          console.log(
-            `Successful document written with ID: ${suffixField}`
+      const genRandAlias = () => {
+        const characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-";
+        let randomAlias = "";
+        for (var i = 0; i < 4; i++) {
+          randomAlias += characters.charAt(
+            Math.floor(Math.random() * characters.length)
           );
-          router.push(`/success`);
-        })
-        .catch((error) => {
-          console.error(`Error adding document: ${error}`);
-        });
-    }
-  }}
+        }
+        firebase
+          .firestore()
+          .collection("links")
+          .get()
+          .then((col) => {
+            col.docs.map((doc) => {
+              if (randomAlias === doc.data().suffix) {
+                return genRandAlias();
+              }
+            });
+          });
+        return randomAlias;
+      };
+
+      if (authAdmins.includes(firebase.auth().currentUser.email)) {
+        const newRandomAlias = genRandAlias();
+        firebase
+          .firestore()
+          .collection(`links`)
+          .doc(suffixField || newRandomAlias)
+          .set({
+            name: nameField ?? "ERROR",
+            link: deepLinkField ?? "ERROR",
+            suffix: suffixField || newRandomAlias,
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+          })
+          .then(() => {
+            createdSuffix = suffixField || newRandomAlias;
+            console.log(`Successful document written with ID: ${suffixField}`);
+            router.push(`/success`);
+          })
+          .catch((error) => {
+            console.error(`Error adding document: ${error}`);
+          });
+      }
+    };
+  }
 
   return (
     <div className={style.main}>
       <h3>Create a new Link</h3>
       <div className={style.newFieldDiv}>
-        <label htmlFor="shortenerName">NAME*</label>
-        <input
-          id="shortenerName"
-          type="text"
+        <ThemeField
+          label="NAME*"
           placeholder="SST Inc Website"
           onChange={(event) => {
             nameField = event.target.value;
@@ -140,11 +138,8 @@ export default function UrlPage(props: {user: User}) {
             }
           }}
         />
-        <label htmlFor="shortenerLink">LONG URL*</label>
-        <input
-          ref={deepLinkFieldRef}
-          id="shortenerLink"
-          type="text"
+        <ThemeField
+          label="LONG URL*"
           placeholder="https://sstinc.org"
           onChange={(event) => {
             deepLinkField = event.target.value;
@@ -156,25 +151,20 @@ export default function UrlPage(props: {user: User}) {
             }
           }}
         />
-        <label htmlFor="shortenerSuffix">SHORT URL</label>
-        <div className={style.shortUrlDiv}>
-          <p className={style.domain}>go.sstinc.org/</p>
-          <input
-            ref={suffixFieldRef}
-            id="shortenerSuffix"
-            type="text"
-            placeholder="sstinc"
-            onChange={(event) => {
-              suffixField = event.target.value;
-            }}
-            onKeyUp={(event) => {
-              if (event.keyCode === 13) {
-                event.preventDefault();
-                createLinkButtonRef.current.click();
-              }
-            }}
-          />
-        </div>
+        <ThemeField
+          label="SHORT URL"
+          placeholder="sstinc"
+          onChange={(event) => {
+            suffixField = event.target.value;
+          }}
+          onKeyUp={(event) => {
+            if (event.keyCode === 13) {
+              event.preventDefault();
+              createLinkButtonRef.current.click();
+            }
+          }}
+          leftDetail={<p className={style.domain}>go.sstinc.org/</p>}
+        />
         <p className={style.smallText}>
           Leaving the field empty would create a randomly generated alias.
         </p>
@@ -187,7 +177,9 @@ export default function UrlPage(props: {user: User}) {
       ) : (
         <></>
       )}
-      <ThemeButton onClick={createUrl} ref={createLinkButtonRef}>Create</ThemeButton>
+      <ThemeButton onClick={createUrl} ref={createLinkButtonRef}>
+        Create
+      </ThemeButton>
       <div className={style.statusOverlay}></div>
     </div>
   );
