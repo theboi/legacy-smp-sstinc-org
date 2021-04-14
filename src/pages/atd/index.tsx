6f6878a009a77efb8c1397161b1257f04c6192c4
Ryan The <ryan.the.2006@gmail.com>
2021-04-14 21:51:37 +0800

    Add dark mode

--- src/pages/atd/index.tsx
+++ src/pages/atd/index.tsx
@@ -8,20 +8,23 @@ import { User, UserRole } from "../../model/user";
 import { FaLock, FaLockOpen } from "react-icons/fa";
 import { fbProvider } from "../../model/fbProvider";
 
-export default function AtdPage(props: { user: User }) {
+import { Button, IconButton, ButtonGroup, Heading } from "@chakra-ui/react";
 
+export default function AtdPage(props: { user: User }) {
   const [time, setTime] = useState(0);
   const [key, setKey] = useState(getKeyCode());
   const [code, setCode] = useState([...Array(4)].map(() => ""));
   const [isLocked, setIsLocked] = useState(true);
-  const [status, setStatus] = useState('Confirm');
+  const [status, setStatus] = useState("Confirm");
 
-  const refs: React.MutableRefObject<HTMLInputElement>[] = [...Array(4)].map(() => useRef(null));
+  const refs: React.MutableRefObject<HTMLInputElement>[] = [
+    ...Array(4),
+  ].map(() => useRef(null));
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(10 - new Date().getSeconds() % 10)
-      return setKey(getKeyCode())
+      setTime(10 - (new Date().getSeconds() % 10));
+      return setKey(getKeyCode());
     }, 1000);
 
     return () => {
@@ -39,33 +42,38 @@ export default function AtdPage(props: { user: User }) {
         break;
       case e.keyCode === 13: // return
         refs[i].current.blur();
-        if (code.join('').length == 4) confirmCode()
+        if (code.join("").length == 4) confirmCode();
         break;
       case e.keyCode === 39: // rightArrow
-        refs[i + 1]?.current.focus()
+        refs[i + 1]?.current.focus();
         break;
     }
   }
 
   function onCodeChange(e: React.ChangeEvent<HTMLInputElement>, i: number) {
-    const val = e.target.value
-    const newChar = val.charAt(val.length - 1)
+    const val = e.target.value;
+    const newChar = val.charAt(val.length - 1);
     const charCode = newChar.charCodeAt(0);
 
-    if (newChar === "") { // backspace
+    if (newChar === "") {
+      // backspace
       setCode((c) => {
         const t = [...c];
         t[i] = "";
         return t;
       });
       // refs[i - 1]?.current?.focus()
-    } else if ((charCode >= 48 && charCode <= 57) || (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+    } else if (
+      (charCode >= 48 && charCode <= 57) ||
+      (charCode >= 65 && charCode <= 90) ||
+      (charCode >= 97 && charCode <= 122)
+    ) {
       setCode((c) => {
         const t = [...c];
         t[i] = newChar.toUpperCase();
         return t;
       });
-      refs[i + 1]?.current?.focus()
+      refs[i + 1]?.current?.focus();
     }
   }
 
@@ -80,22 +88,26 @@ export default function AtdPage(props: { user: User }) {
   function confirmCode() {
     if (code.join("") === getKeyCode()) {
       /** Handle after writing to Firestore */
-      fbProvider.atd.checkIn(props.user).then(() => {
-        setStatus(`Success`)
-      }).catch(e => {
-        setStatus(`Error`)
-      }).finally(() => {
-        setTimeout(() => {
-          setStatus(`Confirm`)
-        }, 2000);
-      })
+      fbProvider.atd
+        .checkIn(props.user)
+        .then(() => {
+          setStatus(`Success`);
+        })
+        .catch((e) => {
+          setStatus(`Error`);
+        })
+        .finally(() => {
+          setTimeout(() => {
+            setStatus(`Confirm`);
+          }, 2000);
+        });
     } else {
       /** Code submission cooldown of 2 sec, informs user that code is incorrect */
-      setStatus(`Invalid`)
-      setCode([...Array(4)].map(() => ""))
-      refs[0].current.focus()
+      setStatus(`Invalid`);
+      setCode([...Array(4)].map(() => ""));
+      refs[0].current.focus();
       setTimeout(() => {
-        setStatus(`Confirm`)
+        setStatus(`Confirm`);
       }, 2000);
     }
   }
@@ -106,42 +118,42 @@ export default function AtdPage(props: { user: User }) {
 
   return (
     <div className={style.main}>
-      <h3>Attendance</h3>
-      <p>Kindly enter the 4 digit code provided to check-in to SST Inc. Your attendance data will be recorded in the SST Inc
-        Attendance Database (SAD).</p>
+      <Heading size="md">Attendance</Heading>
+      <p>
+        Kindly enter the 4 digit code provided to check-in to SST Inc. Your
+        attendance data will be recorded in the SST Inc Attendance Database
+        (SAD).
+      </p>
       <div className={style.code}>
         {(isLocked ? code : key.split("")).map((n, i) => (
           <input
-          ref={refs[i]}
-          value={isLocked ? code[i] : key[i]}
-          type="text"
-          className={style.number}
-          key={i}
-          onKeyDown={(e) => onCodeKeyDown(e, i)}
-          onChange={(e) => onCodeChange(e, i)}
+            ref={refs[i]}
+            value={isLocked ? code[i] : key[i]}
+            type="text"
+            className={style.number}
+            key={i}
+            onKeyDown={(e) => onCodeKeyDown(e, i)}
+            onChange={(e) => onCodeChange(e, i)}
           />
         ))}
         <h1>{!isLocked ? time : null}</h1>
       </div>
       <div className={style.buttons}>
-        <button preset={(() => {
-          switch (status) {
-            case `Error`: case `Invalid`: return "destructive"
-            case `Success`: return "success"
-            default: return "primary"
-          }
-        })()} onClick={confirmCode} disabled={!isLocked || status !== "Confirm" || code.join('').length != 4}>
-          {status}
-        </button>
+        <ButtonGroup isAttached width="100%">
+          <Button
+          isFullWidth
+          onClick={confirmCode}
+          disabled={!isLocked || status !== "Confirm" || code.join("").length != 4}
+          colorScheme={{"Error": "red", "Invalid": "red", "Success": "green"}[status] ?? "blue"}
+          >
+            {status}
+          </Button>
         {props.user?.role >= UserRole.ExCo ? (
-          <div className={style.lock}>
-            <button preset="secondary" onClick={toggleLock}>
-              {isLocked ? <FaLock /> : <FaLockOpen />}
-            </button>
-          </div>
+          <IconButton onClick={toggleLock} aria-label={isLocked ? "Lock" : "Unlock"} icon={isLocked ? <FaLock /> : <FaLockOpen />} />
         ) : null}
+        </ButtonGroup>
       </div>
-      <button preset="secondary" disabled>Scan a QR Code instead</button>
+      <Button disabled>Scan a QR Code instead</Button>
     </div>
   );
 }
---
+++ src/pages/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export default extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+  },
+  fonts: {
+    heading: "Rubik",
+    body: "Rubik",
+  },
+  styles: {
+    global: (props) => ({
+      // "html, body": {
+      //   fontSize: "sm",
+      //   color: props.colorMode === "dark" ? "white" : "gray.600",
+      //   lineHeight: "tall",
+      // },
+      // a: {
+      //   color: props.colorMode === "dark" ? "teal.300" : "teal.500",
+      // },
+    }),
+  },
+});
