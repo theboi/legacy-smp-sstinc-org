fbf8b7799ed47b0a2fceed16b2bbe587ecaa5cc9
Ryan The <ryan.the.2006@gmail.com>
2021-04-17 13:31:20 +0800

    Fix import home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { User } from "../../model/user";
 
-import style from "./style.module.css";
-
 export default function HomePage(props: { user: User }) {
   return (
-    <div className={style.main}>
+    <div>
       
     </div>
   );
