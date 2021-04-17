d59b3f0c649530f9e8cb2c1f55cf291859777be3
Ryan The <ryan.the.2006@gmail.com>
2021-04-17 14:14:09 +0800

    Move theme out of pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ColorModeScript } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
-import theme from "./theme"
+import theme from "../model/theme"
 
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
